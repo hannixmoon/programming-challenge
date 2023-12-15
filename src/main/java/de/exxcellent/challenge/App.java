ce46ceea678eeaecdf1c8b1f79b93752981ccd13
Hanna Blumstein <blumsteinhanna@gmail.com>
2023-12-15 16:27:36 +0100

    Add logic for the CsvReader and opencsv dependency

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,10 +1,16 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.reader.CsvReader;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ * @author Hanna Blumstein <blumsteinhanna@gmail.com>
  */
 public final class App {
 
@@ -14,10 +20,10 @@ public final class App {
      */
     public static void main(String... args) {
 
-        // Your preparation code …
-
         String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+        String weatherFilePath = "src/main/resources/de/exxcellent/challenge/weather.csv";
+        List<Map<String, String>> weatherHashmap = CsvReader.readCsvFile(weatherFilePath);
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
--- src/main/java/de/exxcellent/challenge/reader/CsvReader.java
+++ src/main/java/de/exxcellent/challenge/reader/CsvReader.java
@@ -1,17 +1,55 @@
 package de.exxcellent.challenge.reader;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CsvReader {
 
     public static List<Map<String, String>> readCsvFile(String csvFilePath) {
-        // TODO
-        return convertCsvToMap(null);
+        List<Map<String, String>> dataList = new ArrayList<>();
+
+        try (CSVReader reader = new CSVReader(new FileReader(csvFilePath))) {
+            List<String[]> csvData = reader.readAll();
+
+            if (csvData == null || csvData.size() < 2) {
+                throw new RuntimeException("CSV file is empty or contains only the header");
+            }
+
+            dataList = convertCsvToMap(csvData);
+        } catch (IOException | CsvException e) {
+            System.err.println("Error reading CSV file: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return dataList;
     }
 
-    public static List<Map<String, String>> convertCsvToMap(List<String[]> csvDataList) {
-        //TODO
-        return null;
+    public static List<Map<String, String>> convertCsvToMap(List<String[]> csvData) {
+        List<Map<String, String>> dataList = new ArrayList<>();
+
+        // Assuming the first row contains column headers
+        String[] headers = csvData.get(0);
+
+        for (int i = 1; i < csvData.size(); i++) {
+            String[] row = csvData.get(i);
+            if (row.length != headers.length) {
+                System.err.println("Mismatched number of columns in a row. Skipping the row.");
+                continue;
+            }
+            Map<String, String> rowMap = new HashMap<>();
+
+            for (int j = 0; j < headers.length; j++) {
+                rowMap.put(headers[j], row[j]);
+            }
+
+            dataList.add(rowMap);
+        }
+        return dataList;
     }
 }
