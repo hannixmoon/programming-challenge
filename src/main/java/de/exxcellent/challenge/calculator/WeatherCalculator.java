0ab931272b6890e022ae015bf72f5a2460ae6631
Hanna Blumstein <blumsteinhanna@gmail.com>
2023-12-15 17:05:51 +0100

    Feat: Set up WeatherCalculator and initial Junit Test

---
+++ src/main/java/de/exxcellent/challenge/calculator/WeatherCalculator.java
@@ -0,0 +1,12 @@
+package de.exxcellent.challenge.calculator;
+
+import java.util.List;
+import java.util.Map;
+
+public class WeatherCalculator {
+
+    public static Map<String, String> calcDayWithSmallestTempSpread(List<Map<String, String>> weatherData) {
+
+        return weatherData.get(0);
+    }
+}
---
+++ src/test/java/de/exxcellent/challenge/calculator/WeatherCalculatorTest.java
@@ -0,0 +1,38 @@
+package de.exxcellent.challenge.calculator;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WeatherCalculatorTest {
+
+    @Disabled
+    @Test
+    public void testCalcDayWithSmallestTempSpread() {
+        Map<String, String> weatherMap1 = new HashMap<>();
+        weatherMap1.put("Day", "1");
+        weatherMap1.put("MxT", "88");
+        weatherMap1.put("MnT", "59");
+
+        Map<String, String> weatherMap2 = new HashMap<>();
+        weatherMap2.put("Day", "2");
+        weatherMap2.put("MxT", "79");
+        weatherMap2.put("MnT", "63");
+
+        List<Map<String, String>> weatherData = new ArrayList<>();
+        weatherData.add(weatherMap1);
+        weatherData.add(weatherMap2);
+
+        Map<String, String> result = WeatherCalculator.calcDayWithSmallestTempSpread(weatherData);
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+        assertEquals(weatherMap2, result);
+    }
+}
