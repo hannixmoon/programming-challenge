e2bc7afe24e364893574f41cdfc623ae5d122dbe
hannixmoon <122094214+hannixmoon@users.noreply.github.com>
2023-12-16 15:49:08 +0100

    Refactor: Changed main() to expect CLI arguments

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -18,6 +18,9 @@ import java.util.logging.Logger;
  * @author Hanna Blumstein <blumsteinhanna@gmail.com>
  */
 public final class App {
+    public static final String MISSING_ARGS_STRING = "Please enter valid arguments! Choose " +
+            "[--weather] [filepath] or [--football] [filepath].";
+    public static final String BASE_FILE_PATH = "src/main/resources/de/exxcellent/challenge/";
 
     /**
      * This is the main entry method of your program.
@@ -26,32 +29,36 @@ public final class App {
     public static void main(String... args) {
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-        String baseFilePath = "src/main/resources/de/exxcellent/challenge/";
-        String weatherFilePath = baseFilePath + "weather.csv";
-        String footballFilePath = baseFilePath + "football.csv";
+        if (args.length < 2) {
+            logger.log(Level.INFO, MISSING_ARGS_STRING);
+        } else if (args[0].equals("--weather")) {
+            // Task 1: Weather data
+            try {
+                List<Map<String, String>> weatherDataList = new CsvFileReader().readFile(BASE_FILE_PATH + args[1]);
+                DataCalculator weatherDataCalculator = new WeatherDataCalculator();
+                Map<String, String> dayWithSmallestTempSpread = weatherDataCalculator.calcDataWithMinSpread(weatherDataList);
+                System.out.printf("Day with smallest temperature spread : %s%n",
+                        dayWithSmallestTempSpread.get(WeatherDataCalculator.DAY_COLUMN));
 
-        // Task 1: Weather data
-        try {
-            List<Map<String, String>> weatherDataList = new CsvFileReader().readFile(weatherFilePath);
-            DataCalculator weatherDataCalculator = new WeatherDataCalculator();
-            Map<String, String> dayWithSmallestTempSpread = weatherDataCalculator.calcDataWithMinSpread(weatherDataList);
-            System.out.printf("Day with smallest temperature spread : %s%n",
-                    dayWithSmallestTempSpread.get(WeatherDataCalculator.DAY_COLUMN));
+            } catch (IllegalArgumentException e) {
+                logger.log(Level.SEVERE, "Error while handling weather data");
+            }
 
-        } catch (IllegalArgumentException e) {
-            logger.log(Level.SEVERE, "Error while handling weather data");
-        }
+        } else if (args[0].equals("--football")) {
+            // Task 2: Football data
+            try {
+                List<Map<String, String>> footballDataList = new CsvFileReader().readFile(BASE_FILE_PATH + args[1]);
+                DataCalculator footballDataCalculator = new FootballDataCalculator();
+                Map<String, String> teamWithSmallestGoalSpread = footballDataCalculator.calcDataWithMinSpread(footballDataList);
+                System.out.printf("Team with smallest goal spread       : %s%n",
+                        teamWithSmallestGoalSpread.get(FootballDataCalculator.TEAM_COLUMN));
 
-        // Task 2: Football data
-        try {
-            List<Map<String, String>> footballDataList = new CsvFileReader().readFile(footballFilePath);
-            DataCalculator footballDataCalculator = new FootballDataCalculator();
-            Map<String, String> teamWithSmallestGoalSpread = footballDataCalculator.calcDataWithMinSpread(footballDataList);
-            System.out.printf("Team with smallest goal spread       : %s%n",
-                    teamWithSmallestGoalSpread.get(FootballDataCalculator.TEAM_COLUMN));
+            } catch (IllegalArgumentException e) {
+                logger.log(Level.SEVERE, "Error while handling football data");
+            }
 
-        } catch (IllegalArgumentException e) {
-            logger.log(Level.SEVERE, "Error while handling football data");
+        } else {
+            logger.log(Level.INFO, MISSING_ARGS_STRING);
         }
     }
 }
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,7 +1,11 @@
 package de.exxcellent.challenge;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,8 +28,30 @@ class AppTest {
     }
 
     @Test
+    void runWeather() {
+        App.main("--weather", "weather.csv");
+    }
+
+    @Test
     void runFootball() {
         App.main("--football", "football.csv");
     }
 
+    @Disabled
+    @Test
+    void testInvalidArgs() {
+        // Redirect System.out for testing
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        // Perform calculation with missing arguments
+        App.main();
+        String expectedOutput = App.MISSING_ARGS_STRING;
+
+        // Reset System.out
+        System.setOut(System.out);
+
+        // Verify the output
+        assertEquals(expectedOutput, outContent.toString());
+    }
 }
